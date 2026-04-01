dcfe1e33f7daa57597845524eef408a6d4bf06a7
SeonHaram <hxram06@snu.ac.kr>
2026-04-01 17:35:21 +0900

    modift func

--- main.cpp
+++ main.cpp
@@ -7,16 +7,17 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+   for (int i = 0; i < cnt; i++) {
+      vec.push_back(N);
+      N += M;
+   }
 }
 
 int main() {
    cin >> N >> M;
    func(0);
+   for (int i = 0; i < cnt; i++) {
+      cout << vec[i] << " ";
+   }
+   return 0;
 }
