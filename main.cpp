92713ed85eb02fa0d7bc7794cb50abba1befa3df
SeonHaram <hxram06@snu.ac.kr>
2026-04-06 12:39:31 +0900

    Complete the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
-void func(int cnt) {
-   if (cnt == 0) {
-      for (int i = 1; i <= N; i++) cout << i << " ";
-      return;
-   }
+void func(int start) {
+    if (vec.size() == M) {
+        for (int i = 0; i < M; i++) {
+            cout << vec[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
 
-   int r, q;
-   r = cnt % M;
-   q = cnt / M;
-
-   
-   
+    for (int i = start; i <= N; i++) {
+        vec.push_back(i);
+        func(i + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
-
-   return 0;
+    cin >> N >> M;
+    func(1); 
+    return 0;
 }
